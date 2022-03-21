f43af934840fac7fe3a3152b8a686ce3eeb6e9a0
NothingIsLost <nothingislost@fastmail.net>
2022-03-21 11:03:41 -0700

    fix bug in print dom detection

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,22 @@ export default class PrintPreview extends Plugin {
   private observer: MutationObserver;
 
   onload() {
-    const bodyEl = document.querySelector("body")
+    const bodyEl = document.querySelector("body");
     this.observer = new MutationObserver((mutations, observer) => {
-            mutations.forEach((mutation) => {
-                    mutation.removedNodes.forEach((nodeEl) => {
-                        if ((nodeEl as HTMLElement).className == 'print') {
-                            (nodeEl as HTMLElement).addClass('print-preview');
-                            // (nodeEl as HTMLElement).querySelector(".markdown-preview-view").addClass('markdown-preview-section')
-                            bodyEl.appendChild(nodeEl);
-                        }
-                    });
-            });
+      mutations.forEach(mutation => {
+        mutation.removedNodes.forEach((nodeEl: HTMLElement) => {
+          if (nodeEl.hasClass("print") && !nodeEl.hasClass("print-preview")) {
+            console.log(nodeEl);
+            nodeEl.addClass("print-preview");
+            bodyEl.appendChild(nodeEl);
+          }
+        });
+      });
     });
-    this.observer.observe(bodyEl, { childList: true, subtree: false })
+    this.observer.observe(bodyEl, { childList: true, subtree: false });
   }
 
   onunload() {
-    this.observer.disconnect()
+    this.observer.disconnect();
   }
 }
